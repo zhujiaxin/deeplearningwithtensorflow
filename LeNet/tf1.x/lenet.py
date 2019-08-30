1c1664e86f080ab434f2369b638300911d7f1fcd
Pual Zhu <783725554@qq.com>
2019-08-30 18:20:56 +0800

    add dropout and fix bug

--- LeNet/tf1.x/lenet.py
+++ LeNet/tf1.x/lenet.py
@@ -4,7 +4,12 @@ import numpy as np
 
 
 class LeNet(object):
-    def __init__(self, lr_rate=0.01):
+    """
+    :param lr_rate:初始化学习率，可以使用change_lr_rate函数进行调整
+    :type  lr_rate:float
+    :param regular:L2惩罚系数
+    """
+    def __init__(self, lr_rate=0.01, regular=0.01):
         self.out = None
         self.input_x = None
         self.input_y = None
@@ -17,7 +22,8 @@ class LeNet(object):
             self.input_y = tf.placeholder(tf.float32, [None, 10], name='input_x')
 
         with tf.name_scope('conv1'):
-            kernel1 = tf.Variable(tf.truncated_normal([5, 5, 1, 6], stddev=0.1, dtype=tf.float32), name='weight1')
+            kernel1 = tf.Variable(tf.truncated_normal([5, 5, 1, 6], stddev=0.1, dtype=tf.float32), name='weight1',
+                                  collections=[tf.GraphKeys.GLOBAL_VARIABLES, 'loss'])
             bias1 = tf.Variable(tf.constant(0.0, shape=[6], dtype=tf.float32), trainable=True, name='bias')
             conv1 = tf.nn.conv2d(self.input_x, kernel1, strides=[1, 1, 1, 1], padding='VALID', name='conv')
             out1 = tf.nn.relu(conv1 + bias1)
@@ -27,7 +33,7 @@ class LeNet(object):
 
         with tf.name_scope('conv2'):
             kernel2 = tf.Variable(tf.truncated_normal([5, 5, 6, 16], stddev=0.1, dtype=tf.float32),
-                                  name='conv_weight2')
+                                  name='conv_weight2',collections=[tf.GraphKeys.GLOBAL_VARIABLES, 'loss'])
             bias2 = tf.Variable(tf.constant(0.0, shape=[16], dtype=tf.float32), trainable=True, name='bias')
             conv2 = tf.nn.conv2d(outpool1, kernel2, strides=[1, 1, 1, 1], padding='VALID', name='conv2')
             out2 = tf.nn.relu(conv2 + bias2)
@@ -38,32 +44,42 @@ class LeNet(object):
         with tf.name_scope('fc1'):
             weights1 = tf.Variable(tf.truncated_normal([int(np.prod(outpool2.get_shape()[1:])), 120],
                                                        dtype=tf.float32,
-                                                       stddev=1e-1), name='fc_weights1')
+                                                       stddev=1e-1), name='fc_weights1',
+                                                       collections=[tf.GraphKeys.GLOBAL_VARIABLES, 'loss'])
             bias1_fc = tf.Variable(tf.constant(0.0, shape=[120], dtype=tf.float32),
                                    trainable=True, name='fc_biases1')
-            out3 = tf.nn.relu(
+            fc1 = tf.nn.relu(
                 tf.matmul(tf.reshape(outpool2, [-1, int(np.prod(outpool2.get_shape()[1:]))]), weights1) + bias1_fc)
+            out3 = tf.nn.dropout(fc1, rate=0.5)
 
         with tf.name_scope('fc2'):
             weights2 = tf.Variable(tf.truncated_normal([120, 84],
                                                        dtype=tf.float32,
-                                                       stddev=1e-1), name='fc_weights2')
+                                                       stddev=1e-1), name='fc_weights2',
+                                                       collections=[tf.GraphKeys.GLOBAL_VARIABLES, 'loss'])
             bias2_fc = tf.Variable(tf.constant(0.0, shape=[84], dtype=tf.float32),
                                    trainable=True, name='fc_biases2')
-            out4 = tf.matmul(out3, weights2) + bias2_fc
+            fc2 = tf.nn.relu(tf.matmul(out3, weights2) + bias2_fc)
+            out4 = tf.nn.dropout(fc2, rate=0.5)
 
         with tf.name_scope('outlayer'):
             weights3 = tf.Variable(tf.truncated_normal([84, 10],
                                                        dtype=tf.float32,
-                                                       stddev=1e-1), name='fc_weights3')
+                                                       stddev=1e-1), name='fc_weights3',
+                                                       collections=[tf.GraphKeys.GLOBAL_VARIABLES, 'loss'])
             bias3_fc = tf.Variable(tf.constant(0.0, shape=[10], dtype=tf.float32),
                                    trainable=True, name='fc_biases3')
             self.out = tf.matmul(out4, weights3) + bias3_fc
 
         with tf.name_scope('loss'):
-            regulation_loss = tf.nn.l2_loss(weights1) + tf.nn.l2_loss(weights2)  + tf.nn.l2_loss(weights3)
+            regulation_loss = 0
+            for i in tf.get_collection('loss'):
+                tensor = tf.get_default_graph().get_tensor_by_name(i.name)
+                regulation_loss += tf.nn.l2_loss(tensor)
+            regulation_loss = tf.nn.l2_loss(weights1) + tf.nn.l2_loss(weights2) + tf.nn.l2_loss(weights3)
             self.loss = tf.reduce_mean(
-                tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.input_y, logits=self.out)) + regulation_loss
+                tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.input_y, logits=self.out))\
+                + regulation_loss * regular
 
         self.train_loss_op = tf.train.GradientDescentOptimizer(lr_rate).minimize(self.loss)
 
