0e541a5e18dc47537168182dcd2051a949632e9e
Pual Zhu <783725554@qq.com>
2019-09-08 22:47:07 +0800

    add downloadmodel

---
+++ AlexNet/tf1.x/downloadmodel.py
@@ -0,0 +1,30 @@
+# -*- coding:utf-8 -*-
+import requests
+import os
+import time
+
+
+def download(url, path):
+    if os.path.exists(path):
+        os.remove(path)
+    start = time.time()
+    size = 0
+    respone = requests.get(url, stream=True)
+    chunk_size = 1024
+    content_size = int(respone.headers['Content-Length'])
+    if respone.status_code == 200:
+        print('all size is {:2f} MB'.format(content_size / chunk_size / 1024))
+        with open(path, 'wb') as file:
+            for data in respone.iter_content(chunk_size=chunk_size):
+                file.write(data)
+                size += len(data)
+                print('\r' + 'downloading file:' +
+                      '>'*(size*50 // content_size) +
+                      '{:.2f}% '.format(size*100 / content_size), end='')
+        end = time.time()
+        print('\n download use {:.2f} ms'.format(end-start))
+
+
+
+url = 'http://down.360safe.com/cse/360cse_11.0.2179.0.exe'
+download(url, '1.exe')
